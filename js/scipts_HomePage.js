88b384823d6165d375f0e118dd3a48bb88340d1e
Lê Thắng <lesythang2@gmail.com>
2025-06-13 10:18:14 +0700

    Init

---
+++ js/scipts_HomePage.js
@@ -0,0 +1,2 @@
+fetch('../Composite/header.html').then(response => response.text()).then(data => {document.getElementById('header').innerHTML = data;});
+fetch('../Composite/footer.html').then(response => response.text()).then(data => {document.getElementById('footer').innerHTML = data;});
