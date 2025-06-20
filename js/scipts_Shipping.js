de316051f4657aa5dca222a8886468f4f1aac5ce
Lê Thắng <lesythang2@gmail.com>
2025-06-21 05:28:30 +0700

    Ok cái bảng

---
+++ js/scipts_Shipping.js
@@ -0,0 +1,5 @@
+fetch('/Composite/Shipping/header.html').then(response => response.text()).then(data => {document.getElementById('header').innerHTML = data;});
+fetch('/Composite/Shipping/footer.html').then(response => response.text()).then(data => {document.getElementById('footer').innerHTML = data;});
+fetch('/Composite/Shipping/unpaid.html').then(response => response.text()).then(data => {document.querySelectorAll('#unpaid').forEach(el => {el.innerHTML = data;});});
+fetch('/Composite/Shipping/paid.html').then(response => response.text()).then(data => {document.querySelectorAll('#paid').forEach(el => {el.innerHTML = data;});});
+fetch('/Composite/Shipping/action-button.html').then(response => response.text()).then(data => {document.querySelectorAll('.action-button').forEach(el => {el.innerHTML = data;});});
