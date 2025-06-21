216453c806bff8150182bd0bc4d29f74ef5a7a05
Lê Thắng <lesythang2@gmail.com>
2025-06-21 13:37:13 +0700

    Sửa tí đường dẫn

--- js/scipts_HomePage.js
+++ js/scipts_HomePage.js
@@ -1,2 +1,9 @@
 fetch('../Composite/header.html').then(response => response.text()).then(data => {document.getElementById('header').innerHTML = data;});
 fetch('../Composite/footer.html').then(response => response.text()).then(data => {document.getElementById('footer').innerHTML = data;});
+
+document.body.addEventListener('click', function (e) {
+  const target = e.target.closest('#wallet-icon');
+  if (target) {
+    window.location.href = '/Composite/viDienTu/index.html';
+  }
+});
